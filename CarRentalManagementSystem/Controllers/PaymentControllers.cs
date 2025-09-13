5a9b465901ea00dcb49f76635b7a8069958d21d1
Nilaxs0501 <nobertnilaxshan@gmail.com>
2025-09-13 21:54:03 +0530

    small change

--- CarRentalManagementSystem/Controllers/PaymentControllers.cs
+++ CarRentalManagementSystem/Controllers/PaymentControllers.cs
@@ -52,7 +52,7 @@ namespace CarRentalManagementSystem.Controllers
                 PaymentMethods = new List<SelectListItem>
                 {
                     new SelectListItem { Value = "Cash on Pickup", Text = "Cash on Pickup (Manual)" },
-                    new SelectListItem { Value = "Credit Card", Text = "Credit Card (Online)" }
+                   
                 }
             };
             return View(viewModel);
@@ -100,7 +100,7 @@ namespace CarRentalManagementSystem.Controllers
             viewModel.PaymentMethods = new List<SelectListItem>
                 {
                     new SelectListItem { Value = "Cash on Pickup", Text = "Cash on Pickup (Manual)" },
-                    new SelectListItem { Value = "Credit Card", Text = "Credit Card (Online)" }
+                    
                 };
             return View(viewModel);
         }
