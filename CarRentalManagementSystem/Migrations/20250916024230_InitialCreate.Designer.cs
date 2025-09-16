de06872b88a3487eb9bbf4e8935eedbc9f365479
Nilaxs0501 <nobertnilaxshan@gmail.com>
2025-09-16 10:16:21 +0530

    Customer  manage  Update

--- CarRentalManagementSystem/Migrations/20250916024230_InitialCreate.Designer.cs
+++ CarRentalManagementSystem/Migrations/20250916024230_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRentalManagementSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250915084217_InitialABC")]
-    partial class InitialABC
+    [Migration("20250916024230_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
